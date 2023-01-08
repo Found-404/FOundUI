0bf3e14bd81962e325c79c9edf159742a7836aa1
jinsdw <2856605088@qq.com>
2023-01-08 21:35:03 +0800

    feat: 新增下拉菜单

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Print } from 'FOundUI'
 Print('FOundUI', '1.0.0')
 
 export { default as Button } from './Button'
+export { default as Dropdown } from './Dropdown'
 export { default as Print } from './Print'
 export { default as QRCode } from './QRCode'
--- src/style/primaryColor.ts
+++ src/style/primaryColor.ts
@@ -1,18 +1,21 @@
 /**
- * 主色系
+ * @主色系
  * 1. 蓝色 FOBLUE
  * 2. 红色 FORED
  * 3. 紫色 FOPROP
+ * 4. 灰色 FOGREY
  */
 
 type primaryColorSystemType = {
     FOBLUE: string
     FORED: string
     FOPROP: string
+    FOGREY: string
 }
 
 export const primaryColorSystem: primaryColorSystemType = {
     FOBLUE: '#1976d2',
     FORED: '#fb7676',
-    FOPROP: '#977bf8'
+    FOPROP: '#977bf8',
+    FOGREY: '#D3D3D3'
 }
